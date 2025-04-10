69367e423be144d6f01fcad7437c688948ea7425
mujahid-swe <mujahidulislam.swe@diu.edu.bd>
2025-04-10 11:13:42 +0600

    Update create.php

--- create.php
+++ create.php
@@ -1,65 +1,56 @@
 <?php
-    include "connection.php";
-    if(isset($_POST['submit'])){
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-        $q = " INSERT INTO `crud1`(`name`, `email`, `phone`) VALUES ( '$name', '$email', '$phone' )";
-
-        $query = mysqli_query($conn,$q);
-    }
+  include "connection.php";
+  if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $conn->query("INSERT INTO crud1 (name, email, phone, join_date) VALUES ('$name', '$email', '$phone', NOW())");
+    header("Location: index.php");
+    exit();
+  }
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html>
 <head>
- <title>CRUD</title>
-
+  <title>Create Member</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">PHP CRUD OPERATION</a>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="create.php"><span style="font-size:larger;">Add New</span></a>
-            </li>
-          </ul>
+  <nav class="navbar navbar-dark bg-dark px-3">
+    <a class="navbar-brand" href="index.php">PHP CRUD</a>
+  </nav>
+
+  <div class="container my-5">
+    <div class="col-md-6 mx-auto">
+      <div class="card">
+        <div class="card-header bg-primary text-white text-center">
+          <h4>Create New Member</h4>
+        </div>
+        <div class="card-body">
+          <form method="post">
+            <div class="form-group mb-3">
+              <label>Name</label>
+              <input type="text" name="name" class="form-control" required>
+            </div>
+            <div class="form-group mb-3">
+              <label>Email</label>
+              <input type="email" name="email" class="form-control" required>
+            </div>
+            <div class="form-group mb-3">
+              <label>Phone</label>
+              <input type="text" name="phone" class="form-control" required>
+            </div>
+            <div class="d-flex justify-content-between">
+              <button type="submit" name="submit" class="btn btn-success">Submit</button>
+              <a href="index.php" class="btn btn-secondary">Cancel</a>
+            </div>
+          </form>
         </div>
       </div>
-    </nav>
- <div class="col-lg-6 m-auto">
- 
- <form method="post">
- 
- <br><br><div class="card">
- 
- <div class="card-header bg-primary">
- <h1 class="text-white text-center">  Create New Member </h1>
- </div><br>
-
- <label> NAME: </label>
- <input type="text" name="name" class="form-control"> <br>
-
- <label> EMAIL: </label>
- <input type="text" name="email" class="form-control"> <br>
-
- <label> PHONE: </label>
- <input type="text" name="phone" class="form-control"> <br>
-
- <button class="btn btn-success" type="submit" name="submit"> Submit </button><br>
- <a class="btn btn-info" type="submit" name="cancel" href="index.php"> Cancel </a><br>
-
- </div>
- </form>
- </div>
+    </div>
+  </div>
 </body>
 </html>
+
